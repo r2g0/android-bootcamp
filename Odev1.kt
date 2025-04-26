f0d97ae005dfc9d7dcbd374b118ff713d1f713fa
Arturo <153753728+r2g0@users.noreply.github.com>
2025-04-26 16:57:48 +0300

    Birinci Odev

---
+++ Odev1.kt
@@ -0,0 +1,55 @@
+package com.example.kotlinlessons.variables
+
+fun main() {
+    var sehir = "Aydın"
+    val ulke = "Türkiye"
+    var telefon = "+905555555555"
+    var postaKodu = "09100"
+    var email = "artun@example.com"
+    val meslek = "Yazılım Geliştirici"
+    var stokMiktari = 150
+    var musteriAdi = "Artun Gündüz"
+    var bakiye = 1250.75
+    var dogumGunu = "15-05-1998"
+    val maas = 15000.0
+    val medeniDurum = "Bekar"
+    var urunYorum = "Çok kaliteli bir ürün, tavsiye ederim."
+
+    var odemeTarihi = "2025-02-15"
+    val odeme = 499.99
+    var siparisAdeti = 3
+    val arabaModeli = "Tesla"
+    val kitapAdi = "Kotlin Programlama"
+    val yayinlamaTarihi = "2025-03-13"
+    val indirimMiktari = 25.0
+    val odaSayisi = 3
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val urunAdi = "Termos"
+    val yemekFiyati = 75.50
+
+    val marka = "Apple"
+    val muzikAdi = "Calm Down"
+    val videoSuresi = 3.45
+    var urunPuani = 4.8
+    val resimAdi = "urun_foto.jpg"
+    val dosyaFormati = "JPEG"
+    var renk = "Mavi"
+    var renkKodu = "#0000FF"
+    val telefonModeli = "Galaxy S23"
+    val ekranBoyutu = 6.1
+    val agirlik = 168.0
+
+    val ulusalGun = "29 Ekim"
+    val tatilGunu = "1 Mayıs"
+    val rezervasyonTarihi = "2025-09-26"
+    var sokakAdi = "Bağdat Caddesi"
+    val otobusHatti = "500T"
+    var kalanDakika = 15
+    val takipKodu = "TR123456789"
+    val kuponSuresi = 7
+    val kuponKodu = "SLM2025"
+    var faturaAdresi = "Mimar Sinan Mah. No:123 Efeler/Aydın"
+
+
+}
